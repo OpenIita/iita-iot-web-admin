40428f7119299046222874893a03ca05ae6fa7aa
jay <75509151@qq.com>
2024-06-05 11:35:53 +0800

    update tsl 物模型-json直接导入 update productKey允许手动输入

--- src/views/iot/equipment/products/index.vue
+++ src/views/iot/equipment/products/index.vue
@@ -172,7 +172,7 @@ const column = ref<IColumn[]>([
     key: 'productKey',
     search: true,
     editDisabled: true,
-    addDisabled: true,
+    addDisabled: false,
     rules: [{ required: true, message: '产品Key不能为空' }],
   },
   {
--- src/views/iot/equipment/products/modules/tsl.vue
+++ src/views/iot/equipment/products/modules/tsl.vue
@@ -1,16 +1,35 @@
 <template>
-  <vue-json-editor v-model:json="model" :showBtns="false" :mode="'code'" lang="zh" height="400" />
+  <div>
+    <vue-json-editor v-model:modelValue="model" :showBtns="true" :mode="'code'" lang="zh" height="400" />
+    <el-button @click="submitThingModelChange()">保存</el-button>
+  </div>
 </template>
 <script lang="ts" setup>
 import { propTypes } from '@/utils/propTypes'
 import vueJsonEditor from 'vue3-ts-jsoneditor'
+import { saveObjectModel } from '@/views/iot/equipment/api/products.api';
 
 const props = defineProps({
+  id: propTypes.string.def(''),
   model: propTypes.object.def({}),
 })
+
 
 const model = ref(props.model)
 watch(() => props.model, (newV) => {
   model.value = newV
 })
+
+const submitThingModelChange = () => {
+  if (!model) {
+    return
+  }
+  saveObjectModel({
+    productKey: props.id,
+    model:JSON.stringify( model.value),
+  }).then(() => {
+
+  }).finally(()=>{
+  })
+}
 </script>
