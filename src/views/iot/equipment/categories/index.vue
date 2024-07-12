37f37925b34f145a6575a314aeefacc0dd6d7260
jay <75509151@qq.com>
2024-07-12 11:37:36 +0800

    fix 品类查询

--- src/views/iot/equipment/categories/index.vue
+++ src/views/iot/equipment/categories/index.vue
@@ -75,7 +75,7 @@ const onDelete = async (row: any) => {
 // 获取数据
 const getData = async () => {
   state.loading = true
-  getCategoriesList(state.page).then(res => {
+  getCategoriesList({...state.page, ...state.query}).then(res => {
     data.value = res.data.rows
     state.total = res.data.total
   }).finally(() => {
